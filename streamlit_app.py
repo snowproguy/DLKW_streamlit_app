9dae8c6509f5e7870cc89c44efd71128cac97bb4
Guy Harris <111207078+snowproguy@users.noreply.github.com>
2022-08-18 09:30:17 -0400

    Update streamlit_app.py
    correct new line issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import pandas
 # Main body of code:
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) my_cur = my_cnx.cursor()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
+my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog my_cur.execute("select color_or_style from catalog_for_website") my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
