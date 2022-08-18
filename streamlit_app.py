c9ac8fdd12912b96b8c93f436b823556f336bd9f
Guy Harris <111207078+snowproguy@users.noreply.github.com>
2022-08-18 09:38:37 -0400

    Update streamlit_app.py
    correcting additional new line issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) 
 my_cur = my_cnx.cursor()
 # run a snowflake query and put it all in a var called my_catalog 
-my_cur.execute("select color_or_style from catalog_for_website") my_catalog = my_cur.fetchall()
+my_cur.execute("select color_or_style from catalog_for_website") 
+my_catalog = my_cur.fetchall()
 # put the dafta into a dataframe
 df = pandas.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with # 
